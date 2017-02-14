7b44f7c4d7a0b871c34f2a20ed240fd6784a04e7
kartikbhojak <bhojak.kartik72@gmail.com>
2017-02-15 01:39:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,12 @@ var element=document.getElementById('main text'
 
 element.innerHTML='New value';
 
-var img=document.getElementById('img');
-img.oneclick=function(){
-    img.style.marginLeft='100px';
+var img=document.getElementById('modi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft + 'px';
+}
+img.oneClick=function(){
+    var interval=setInterval(moveRight,1);
 };
