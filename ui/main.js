b2a6a55f08fda3e5d38d3eb7af3d37132d0cda3b
kartikbhojak <bhojak.kartik72@gmail.com>
2017-02-15 23:13:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,3 +27,19 @@ button.oneClick = function() {
     request.send(null);
     
 };
+//Submit name
+var nameInput=document.getElementById('name');
+var name = nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.oneClick=function(){
+    // Make a request to the server and send the name
+    //capture a list of names
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0; i<names.length; i++){
+        list+='<li>'+names[i] + '<li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+    
+};
