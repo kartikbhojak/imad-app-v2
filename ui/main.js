25616866d83977eff3f457c7253aab968248b418
kartikbhojak <bhojak.kartik72@gmail.com>
2017-02-15 22:24:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,8 @@
 //counter code
 var button=document.getElementById('counter');
-
 var counter=0;
 
-button.oneClick=function(){
+button.oneClick = function() {
     // MAke a request to the counter
     
     //Capture the variable
